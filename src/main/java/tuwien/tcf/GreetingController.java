61b9d619ced54b269a1c16fa4fb21bb299f1eb4f
Amin Mirdamadi <amin.mtehrani@gmail.com>
2017-01-31 07:06:49 +0100

    version 0.1

--- src/main/java/tuwien/tcf/GreetingController.java
+++ src/main/java/tuwien/tcf/GreetingController.java
@@ -5,13 +5,14 @@
  */
 package tuwien.tcf;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.hateoas.mvc;
 /**
  *
  * @author Amin
@@ -27,7 +28,7 @@ public class GreetingController {
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 
         Greeting greeting = new Greeting(String.format(TEMPLATE, name));
-        greeting.add(linkTom(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        //greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
 
         return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
